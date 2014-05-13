96302f40dadc71704608738ce33903878a7f977a
Dezl <demozluk@gmail.com>
2014-05-13 12:42:49 +0400

    minor changes

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,7 +2,8 @@ module GroupsHelper
   def menu_for_group product_group
     path = group_path(pid: product_group.parent.try(:permalink), id: product_group.permalink)
     content_tag :li, class: "#{product_group.children.any? ? 'parent' : nil} #{ current_page?(path) ? 'current' : nil}" do
-      link_to(product_group.title, path) +
+      string = ": #{children.map { |c| c.title }.join(', ')}" if children.any?
+      link_to(product_group.title, path), title: (product_group.title + string.to_s) +
       if product_group.children.any?
         ul = content_tag :ul, class: 'child' do
           product_group.children.collect do |child|
--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module Fishmarkt
     config.encoding = "utf-8"
     
     config.i18n.enforce_available_locales = true
+    config.assets.precompile += ['jquery.js']
   end
 end
